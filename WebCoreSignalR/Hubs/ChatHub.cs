f250a444c047436f430a54674cd01ca0b499c6ae
doktor alban <doktoralban@outlook.com>
2020-06-28 16:43:50 +0300

    Initial commit

---
+++ WebCoreSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebCoreSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
+
+
+
+    }
+}
